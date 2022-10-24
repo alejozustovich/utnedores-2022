cbcf12c7c75412b92f10c0e36d2bd2921052bf20
Daniel <guelerdaniel@gmail.com>
2022-10-23 23:51:23 -0300

    Camara y lector qr

--- utnedores/src/app/encuesta-empleados/encuesta-empleados.page.ts
+++ utnedores/src/app/encuesta-empleados/encuesta-empleados.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService, Usuario } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-encuesta-empleados',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EncuestaEmpleadosPage implements OnInit {
 
-  constructor() { }
+  users: Usuario[];
+  perfil = "";
+  tipo = "";
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) {
+      setTimeout(()=>{
+       this.GuardarPerfil();
+      },3000);
+   }
 
   ngOnInit() {
+
   }
 
+  GuardarPerfil(){
+    var usuarioLogueado = this.authService.usuarioActual();
+    setTimeout(()=>{
+      this.authService.getUsers().subscribe(allUsers => {
+        this.users = allUsers;
+        for(var i = 0 ; i < allUsers.length ; i++)
+        {
+          if(((this.users[i].correo).toLocaleLowerCase()).includes((usuarioLogueado.toLocaleLowerCase()))) {
+            this.perfil = this.users[i].perfil;
+            this.tipo = this.users[i].tipo;
+            i = allUsers.length;
+          }
+        }
+      });
+     },1000);
+	}
+
+  SaltarEncuesta(){
+    if(this.perfil.includes("Dueño") || this.perfil.includes("Supervisor")){
+      this.router.navigateByUrl('/home', { replaceUrl: true });
+    }else{
+      if(this.perfil.includes("Cliente")){
+        this.router.navigateByUrl('/home-cliente', { replaceUrl: true });
+      }else{
+        if(this.perfil.includes("Empleado")){
+          if(this.tipo.includes("Metre")){
+            this.router.navigateByUrl('/home-metre', { replaceUrl: true });
+          }else{
+            if(this.tipo.includes("Mozo")){
+              this.router.navigateByUrl('/home-mozo', { replaceUrl: true });
+            }else{
+              if(this.tipo.includes("Bartender") || this.tipo.includes("Cocinero")){
+                this.router.navigateByUrl('/home-cocina', { replaceUrl: true });
+              }else{
+                //Error
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
