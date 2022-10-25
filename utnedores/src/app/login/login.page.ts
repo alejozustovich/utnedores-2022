e485761c60c48fad967a404945ed1175d07db743
Ignacio Molini <ignacio.molini25@gmail.com>
2022-10-24 22:56:50 -0300

    acceso rapido login

--- utnedores/src/app/login/login.page.ts
+++ utnedores/src/app/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService, Usuario } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -20,12 +21,13 @@ export class LoginPage implements OnInit {
   constructor(
     private authService: AuthService,
     private fb: FormBuilder,
-    private router: Router
-    ) {
-      this.TraerUsuarios();
-    }
+    private router: Router,
+    private toastController: ToastController
+  ) {
+    this.TraerUsuarios();
+  }
 
-  TraerUsuarios(){
+  TraerUsuarios() {
     this.authService.getUsers().subscribe(allUsers => {
       this.users = allUsers;
     });
@@ -34,44 +36,63 @@ export class LoginPage implements OnInit {
   ngOnInit() {
     this.formLogin = this.fb.group(
       {
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required, Validators.minLength(6)]]
+        correo: ['', [Validators.required, Validators.email]],
+        clave: ['', [Validators.required, Validators.minLength(6)]]
       }
     )
   }
 
-  get email() {
-    return this.formLogin.get('email');
+  get correo() {
+    return this.formLogin.get('correo');
   }
 
-  get password() {
-    return this.formLogin.get('password');
+  get clave() {
+    return this.formLogin.get('clave');
   }
 
-  async iniciarSesion(){
+  async presentToast(message: string, color: string) {
+    const toast = await this.toastController.create({
+      message,
+      color,
+      duration: 2500
+    });
+    toast.present();
+  }
 
-    const user = await this.authService.login(this.formLogin.value);
-    
-    if(user){
-      for(var i = 0 ; i < this.users.length ; i++)
-      {
-        if(((this.users[i].correo).toLocaleLowerCase()).includes((this.formLogin.value.email.toLocaleLowerCase()))) {
+  customAlertOptions = {
+    header: 'Elija un usuario',
+    translucent: true
+  };
+
+  llenarCampos(event) {
+    this.correo.setValue(this.users[event.target.value].correo);
+    this.clave.setValue(this.users[event.target.value].clave);
+  }
+
+  async iniciarSesion() {
+    const data = { email: this.correo.value, password: this.clave.value }
+    const user = await this.authService.login(data);
+    if (user) {
+      for (var i = 0; i < this.users.length; i++) {
+        if (((this.users[i].correo).toLocaleLowerCase()).includes((this.correo.value.toLocaleLowerCase()))) {
           this.perfil = this.users[i].perfil;
+          this.presentToast(`Bienvenido ${this.users[i].nombre}!`, 'success');
+          break;
         }
       }
-  
-      if(this.perfil.includes("Dueño") || this.perfil.includes("Supervisor")){
+      if (this.perfil.includes("Dueño") || this.perfil.includes("Supervisor")) {
         this.router.navigateByUrl('/home', { replaceUrl: true });
-      }else{
-        if(this.perfil.includes("Cliente")){
+      } else {
+        if (this.perfil.includes("Cliente")) {
           this.router.navigateByUrl('/home-cliente', { replaceUrl: true });
-        }else{
+        } else {
           this.router.navigateByUrl('/encuesta-empleados', { replaceUrl: true });
         }
       }
+     
     }
-    else{
-      //MENSAJE ERROR
+    else {
+      this.presentToast('Correo o clave incorrecto/a!', 'danger');
     }
   }
 
