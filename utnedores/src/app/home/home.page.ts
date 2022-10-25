9a881f766cc83420b7d2243d75a773d42cd3bb8d
Alejo Acuña Zustovich <47436065+alejozustovich@users.noreply.github.com>
2022-10-24 22:10:31 -0300

    Métodos Home Supervisor

--- utnedores/src/app/home/home.page.ts
+++ utnedores/src/app/home/home.page.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  volumenOn = true;
+  volumenOff = false;
+  spinner = false;
+  perfil = "Dueño"; //Cambia a Supervisor dependiendo el caso.
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  ActivarDesactivarSonido() {
+    if(this.volumenOn) {
+      this.volumenOn = false;
+      this.volumenOff = true;
+    } else {
+      this.volumenOn = true;
+      this.volumenOff = false;
+    }
   }
 
 }
+
