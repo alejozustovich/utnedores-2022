84e9fb4870a2a7acf9da70d7a9e8be69d4b1a96f
alejozustovich <alejozustovich@gmail.com>
2022-10-30 17:01:14 -0300

    home mozo pedidos

--- utnedores/src/app/home-mozo/home-mozo.page.ts
+++ utnedores/src/app/home-mozo/home-mozo.page.ts
@@ -1,3 +1,6 @@
+import { UtilidadesService } from '../services/utilidades.service';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeMozoPage implements OnInit {
 
-  constructor() { }
+  volumenOn = true;
+  spinner = false;
+  hayPedidos = false;
 
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private utilidades: UtilidadesService
+  ) { 
+    this.Sonido();
+  }
+
+  ngOnInit() { }
+
+  Sonido(){
+    try {
+      var sonido = localStorage.getItem('sonido');
+      if(sonido != null){
+        if(sonido.includes("No")){
+          this.volumenOn = false;
+        }
+      }
+    } catch (error) {
+      
+    }
+  }
+
+  ActivarDesactivarSonido() {
+    if(this.volumenOn) {
+      this.volumenOn = false;
+      localStorage.setItem('sonido', "No");
+    } else {
+      this.volumenOn = true;
+      localStorage.setItem('sonido', "Si");
+    }
+  }
+
+  SonidoEgreso(){
+    if(this.volumenOn) {
+      this.utilidades.PlayLogout();
+    }
+    localStorage.clear();
+  }
+
+  ActivarSpinner(){
+    this.spinner = true;
+  }
+
+  CerrarSesion(){
+    this.ActivarSpinner();
+    this.SonidoEgreso();
+    this.authService.logout();
+    this.router.navigateByUrl('/login', { replaceUrl: true });
+  }
+
+  VerPedidos() {
   }
 
 }
